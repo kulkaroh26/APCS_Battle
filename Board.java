6a1ee896dd85818f5a45c7a1ea9826d03746ebfe
kulkaroh26 <114490265+kulkaroh26@users.noreply.github.com>
2024-10-30 09:34:50 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,1 +1,15 @@
+//Board File
+
+//Whats in the box
+//5 ships
+	//Carriers have 5 pegs
+//2 boards
+	//rows and columns
+	//10x10
+	//letters down the side
+	//numbers across top
+//2 colored sets of pegs
+	//red peg for hit
+	//white peg for miss
+	
 
